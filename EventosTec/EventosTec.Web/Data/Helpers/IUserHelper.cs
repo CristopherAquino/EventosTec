5e4bda0221c2e82f4c013581f8518556ce9b89ef
CristopherAquino <60916859+CristopherAquino@users.noreply.github.com>
2020-05-13 22:30:59 -0700

    Users

---
+++ EventosTec/EventosTec.Web/Data/Helpers/IUserHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventosTec.Web.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace EventosTec.Web.Data.Helpers
+{
+    interface IUserHelper
+    {
+        Task<User> GetUserByEMailAsync(string email);
+        Task<IdentityResult> AddUserAssync(User user, string Password);
+        Task CheckRoleAsync(string roleName);
+        Task AddUserToRoleAsync(User user, string roleName);
+
+        Task<bool> IsUserInRoleAsync(User user, string roleName);
+
+    }
+}
---
+++ EventosTec/EventosTec.Web/Models/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventosTec.Web.Models.Entities
+{
+    public class User : IdentityUser
+    {
+        [Display(Name ="Nombre Completo")]
+        [MaxLength(150)]
+        public string FullName { get; set; }
+
+        [Display(Name = "Descripcion")]
+        [MaxLength(300)]
+        public string Description { get; set; }
+
+        [Display(Name = "Foto")]
+        public string Photo { get; set; }
+
+        [Display(Name = "Fecha de Nacimiento")]
+        public DateTime? Birthdate { get; set; }
+
+    }
+}
