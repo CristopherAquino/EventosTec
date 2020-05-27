7c80c1a33a90d9caa13cf370321ddca8a75e1bb4
CristopherAquino <60916859+CristopherAquino@users.noreply.github.com>
2020-05-27 13:21:55 -0700

    EventosAdmin

--- EventosTec/EventosTec.Web/Controllers/EventsController.cs
+++ EventosTec/EventosTec.Web/Controllers/EventsController.cs
@@ -65,7 +65,21 @@ namespace EventosTec.Web.Controllers
                 ViewData["CityId"] = new SelectList(_context.Cities, "Id", "Name");
                 ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
                 return View();
-    
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateEvent(Event @event)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(@event);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.ClientId = _context.Clients.Include(u => u.User).ToList();
+            ViewData["CityId"] = new SelectList(_context.Categories, "Id", "Name", @event.CategoryId);
+            return View();
         }
 
 
